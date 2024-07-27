c0e6e43dc11a65cc478e39a3cc4ef83171fdb398
javed147 <javadnb@code.edu.az>
2024-07-28 03:07:47 +0530

    Added Adminpanel dashboard and Order controller controller

--- TamVaxti/Areas/Admin/Controllers/DashboardController.cs
+++ TamVaxti/Areas/Admin/Controllers/DashboardController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using TamVaxti.Data;
+using TamVaxti.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Text.Json;
 
 namespace TamVaxti.Areas.Admin.Controllers
 {
@@ -7,10 +12,74 @@ namespace TamVaxti.Areas.Admin.Controllers
     //[Authorize(Roles = "SuperAdmin, Admin")]
     public class DashboardController : Controller
     {
-        
-        public IActionResult Index()
+        private AppDbContext _context;
+        public IDashboardService _dashboardService;
+        public DashboardController(AppDbContext context, IDashboardService dashboardService)
         {
+            _context = context;
+            _dashboardService = dashboardService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+            var OrdersInMonth = await _dashboardService.GetAllProductsPurchasedInMonthAsync(DateTime.Now);
+            //return Ok(OrdersInMonth);
+            ViewBag.Earnings = OrdersInMonth.Sum(o => o.TotalAmount);
+            ViewBag.TotalSku = OrdersInMonth.Sum(o => o.OrderProductDetails.Sum(od => od.Quantity));
+            ViewBag.OrderCnt = OrdersInMonth.Count;
+
+            var LatestOrders = await _dashboardService.GetLatestOrdersInMonthAsync(DateTime.Now, 5);
+            if (LatestOrders != null)
+            {
+                ViewBag.LatestOrders = LatestOrders.Select(o => new
+                {
+                    OrderId = o.OrderId,
+                    TotalAmount = o.TotalAmount,
+                    DeliveryStatus = o.DeliveryStatus,
+                    PaymentMode = o.Payments?.FirstOrDefault().PaymentMode
+                });
+            }
+
+            var LastMonth = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1).AddMonths(-1);
+            var OrdersInLastMonth = await _dashboardService.GetAllProductsPurchasedInMonthAsync(LastMonth);
+
+            ViewBag.LastMonth = LastMonth.ToString("MMMM");
+            ViewBag.LastMonthEarnings = OrdersInLastMonth.Sum(o => o.TotalAmount);
+
+            var CashTransactions = OrdersInMonth.Select(o => new
+            { TotalAmount = o.TotalAmount, PaymentMode = o.Payments.FirstOrDefault().PaymentMode });
+
+            ViewBag.CashTransactions = CashTransactions.Where(p => p.PaymentMode == "Cash").Sum(o => o.TotalAmount);
+            //return Ok(ViewBag.LatestOrders);
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetLastMonthRevenue()
+        {
+            var startDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month - 1, 1);
+
+            var orders = await _dashboardService.GetAllProductsPurchasedInMonthAsync(startDate);
+
+            var ordernw = orders.GroupBy(o => o.OrderDate).Select(g => new
+            {
+                Date = g.Key,
+                TotalAmount = g.Sum(o => o.TotalAmount)
+            }).OrderBy(o => o.Date).ToList();
+
+            var labels = ordernw.Select(o => o.Date.Day).ToList();
+            var series = ordernw.Select(o => o.TotalAmount).ToList();
+
+            var nestedList = new List<List<decimal>> { series };
+
+            var model = new
+            {
+                Labels = labels,
+                Series = nestedList
+            };
+
+            return Ok(JsonSerializer.Serialize(model));
         }
     }
 }
---
+++ TamVaxti/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,173 @@
+﻿using TamVaxti.Data;
+using TamVaxti.Models;
+using TamVaxti.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
+using TamVaxti.ViewModels.Orders;
+using TamVaxti.Helpers;
+
+namespace TamVaxti.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OrderController : Controller
+    {
+        private AppDbContext _context;
+        private readonly UserManager<AppUser> _userManager;
+        public IOrderService _orderService;
+        public OrderController(AppDbContext context, UserManager<AppUser> userManager, IOrderService orderService)
+        {
+            _context = context;
+            _userManager = userManager;
+            _orderService = orderService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(int page = 1)
+        {
+            dynamic orders = await _orderService.GetAllOrdersPaginatedAsync(page, 100);
+
+            int pageCount = await GetPageCountAsync(4);
+
+            Paginate<Order> model = new(orders, pageCount, page);
+            /*orders = from order in _context.Orders
+                        join user in _userManager.Users.ToList()
+                        on order.CustomerId equals user.Id
+                        select new
+                        {
+                            Order = order,
+                            UserName = user.UserName,
+                            Payments = order.Payments.ToList()
+                        };
+            var res = await orders.ToListAsync();*/
+            return View(orders);
+        }
+
+        private async Task<int> GetPageCountAsync(int take)
+        {
+            int count = await _orderService.GetCountAsync();
+
+            return (int)Math.Ceiling((decimal)count / take);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Tracking(int id)
+        {
+            var order = _orderService.GetOrderById(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            order.OrderTracking = await _orderService.GetTrackingDetails(id);
+            var DeliveryStatusItems = _orderService.GetDeliveryStatusItems();
+            DeliveryStatusItems.Remove("Returned");
+            DeliveryStatusItems.Remove("Cancelled");
+            ViewData["DeliveryStatusItems"] = DeliveryStatusItems;
+
+            return View(order);
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var order = _orderService.GetOrderById(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            ViewData["DeliveryStatusItems"] = _orderService.GetDeliveryStatusItems();
+
+            return View(order);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var order = _orderService.GetOrderById(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            var model = new OrderEditVM
+            {
+                OrderId = order.OrderId,
+                DeliveryStatus = order.DeliveryStatus,
+                EstimatedDeliveryDate = order.EstimatedDeliveryDate,
+                ActualDeliveryDate = order.ActualDeliveryDate,
+                ExpectedDeliveryDate = order.ExpectedDeliveryDate
+            };
+
+            ViewData["DeliveryStatusItems"] = _orderService.GetDeliveryStatusItems();
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(OrderEditVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = _orderService.UpdateOrder(model);
+                if (result)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                ModelState.AddModelError("", "Unable to update order.");
+            }
+            return View(model);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var order = _orderService.GetOrderById(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            var result = _orderService.DeleteOrder(id);
+            if (result)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            return RedirectToAction(nameof(Index), new { id });
+        }
+
+        [HttpGet]
+        public IActionResult AddTracking(int id)
+        {
+            var model = new OrderTrackingVM { OrderId = id };
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult AddTracking(OrderTrackingVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                var order = new OrderTracking
+                {
+                    OrderId = model.OrderId,
+                    Location = model.Location,
+                    Notes = model.Notes,
+                    Timestamp = model.Timestamp
+                };
+                var res = _orderService.AddTracking(order);
+                if (res)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return RedirectToAction(nameof(Index));
+            
+        }
+
+    }
+}
