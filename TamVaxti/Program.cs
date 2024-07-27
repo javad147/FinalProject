67b648e233c9239741efee827b38cab24c734558
javed147 <javadnb@code.edu.az>
2024-07-28 03:37:56 +0530

    Added all the  Dependency injection of Interface and services

--- TamVaxti/Program.cs
+++ TamVaxti/Program.cs
@@ -1,11 +1,15 @@
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using TamVaxti.Data;
 using TamVaxti.Models;
-using TamVaxti.Services.Interfaces;
 using TamVaxti.Services;
+using TamVaxti.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
+
+
 
 builder.Services.AddDbContext<AppDbContext>(options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
@@ -17,8 +21,15 @@ builder.Services.AddControllersWithViews().AddJsonOptions(options =>
     options.JsonSerializerOptions.MaxDepth = 64; // Increase the max depth if needed
 });
 
-builder.Services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>()
-                                                     .AddDefaultTokenProviders();
+
+
+//builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
+//{
+//    options.LoginPath = "/Account/Login"; // Custom login page
+//    options.AccessDeniedPath = "/Account/AccessDenied"; // Custom access denied page
+//});
+
+
 builder.Services.Configure<IdentityOptions>(opt =>
 {
     opt.Password.RequiredUniqueChars = 1;
@@ -30,9 +41,23 @@ builder.Services.Configure<IdentityOptions>(opt =>
     opt.User.RequireUniqueEmail = true;
 });
 
-builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IExpertService, ExpertService>();
+builder.Services.AddScoped<ISliderInstaService, SliderInstaService>();
+builder.Services.AddScoped<IBlogService, BlogService>();
+builder.Services.AddScoped<ISettingService, SettingService>();
+builder.Services.AddScoped<ISubcategoryService, SubCategoryService>();
 builder.Services.AddScoped<ICompanyService, CompanyService>();
+builder.Services.AddScoped<INewsletterService, NewsletterService>();
+builder.Services.AddScoped<IAttributeService, AttributeService>();
+builder.Services.AddScoped<IAttributeOptionService, AttributeOptionService>();
+builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddScoped<IContactUsService, ContactUsService>();
 builder.Services.AddScoped<IAboutUsService, AboutUsService>();
+builder.Services.AddScoped<IUserWishList, UserWishListService>();
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddScoped<IDashboardService, DashboardService>();
 
 var app = builder.Build();
 
@@ -44,10 +69,15 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+
+
+app.UseAuthentication();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
+
+
 
 app.MapControllerRoute(
   name: "areas",
